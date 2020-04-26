3d7de33c4b9ec7a533a2096f1f8b94ddd8dd7f5a
Saurabh Dhingra <saurabh.d2106@gmail.com>
2020-04-26 20:18:27 +0530

    Updated Extent Report to the Framework

--- AmazonTests/Amazon.Tests/AmazonHomePageTests.cs
+++ AmazonTests/Amazon.Tests/AmazonHomePageTests.cs
@@ -19,57 +19,57 @@ namespace AmazonTests.Amazon.Tests
         AmazonHomePage amazonHomePage;
         CommonDriver cmnDriver;
 
-        ExtentHtmlReporter HtmlReporter;
-        ExtentReports extentReports;
-        ExtentTest extentTest;
+        ExtentReport extentReport;
 
         DataTable testData;
 
         [SetUp]
         public void InvokeBrowser()
         {
-            HtmlReporter = new ExtentHtmlReporter("/Reports/TestHtmlReport.html");
-            extentReports = new ExtentReports();
 
-            extentReports.AttachReporter(HtmlReporter);
+            extentReport = new ExtentReport("/Reports/testHtmlReport.html");
 
-            extentTest = extentReports.CreateTest("Setup - Invoke Browser");
+            extentReport.CreateATestCase("Setup - Setting up the pre-requisites for the test cases");
 
-            extentTest.Log(Status.Info, "Browser Initiated is - Chrome");
+            extentReport.AddTestLog(Status.Info, "Initializing chrome browser");
             cmnDriver = new CommonDriver("chrome");
 
-            extentTest.Log(Status.Info, "Initializing all classes");
+            extentReport.AddTestLog(Status.Info, "Initializing all web pages");
             amazonHomePage = new AmazonHomePage(cmnDriver.Driver);
 
 
-           testData = ExcelDriver.ReadDataFromExcel("D:/Users/Saurabh Dhingra/source/repos/Learning Selenium/Amazon Application/TestData/InputTestData.xlsx");
+            testData = ExcelDriver.ReadDataFromExcel("D:/Users/Saurabh Dhingra/source/repos/Learning Selenium/Amazon Application/TestData/InputTestData.xlsx");
         }
 
         [Test]
         public void SearchProduct()
         {
-            extentTest = extentReports.CreateTest("Testcase 1 - Search Product");
-
-            extentTest.Log(Status.Info, "Search Product - IPhone and Category - Electronics");
-
+            extentReport.CreateATestCase("Test case 001 - Search Product Functionality");
 
 
             string product = "IPhone";
-            string category = "Electronics";
+            extentReport.AddTestLog(Status.Info, "Product searched - " + product);
 
-            cmnDriver.NavigateToFirstUrl("https://www.amazon.in");
+            string category = "Electronics";
+            extentReport.AddTestLog(Status.Info, "Category searched - " + category);
+
+            string url = "https://www.amazon.in";
+
+            cmnDriver.NavigateToFirstUrl(url);
+            extentReport.AddTestLog(Status.Info, "Navigating to URL - " + url);
+
 
             amazonHomePage.SearchProduct(product, category);
-
+            extentReport.AddTestLog(Status.Info, "Searching Product done");
 
         }
 
         [TearDown]
         public void CloseBrowser()
         {
-            extentTest = extentReports.CreateTest("Tear Down - Close Browser");
+            extentReport.AddTestLog(Status.Info, "Closing all Browsers");
             cmnDriver.CloseAllBrowsers();
-            extentReports.Flush();
+            extentReport.FlushExtentReports();
         }
     }
 }
---
+++ CommonLibs/Utils/ExtentReport.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+
+namespace CommonLibs.Utils
+{
+    public class ExtentReport
+    {
+        ExtentHtmlReporter HtmlReporter;
+        ExtentReports extentReports;
+        ExtentTest extentTest;
+
+        public ExtentReport(string htmlReportFilename)
+        {
+            HtmlReporter = new ExtentHtmlReporter("/Reports/TestHtmlReport.html");
+            extentReports = new ExtentReports();
+
+            extentReports.AttachReporter(HtmlReporter);
+        }
+
+        public void CreateATestCase(string testcasename)
+        {
+            extentTest = extentReports.CreateTest(testcasename);
+        }
+
+        public void AddTestLog(Status status, string comments)
+        {
+            extentTest.Log(status, comments);
+        }
+
+        public void FlushExtentReports()
+        {
+            extentReports.Flush();
+        }
+    }
+}
